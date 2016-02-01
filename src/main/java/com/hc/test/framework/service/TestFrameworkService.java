06a4120c7387afa57acaf3e0e82f1421d352dcc9
sabyasachi <sabyasachi.mishra@helpchat.in>
2016-02-01 18:34:44 +0530

    added condition for linux

--- src/main/java/com/hc/test/framework/service/TestFrameworkService.java
+++ src/main/java/com/hc/test/framework/service/TestFrameworkService.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -52,7 +53,10 @@ public class TestFrameworkService {
         {
             os = "Mac";
         
-        }else{
+        }else if(osType.toLowerCase().indexOf("linux")>=0){
+			os="Linux";
+		}
+		else{
             os = "UnKnown, More-Info: "+osType;
         }
 		
@@ -77,5 +81,4 @@ public class TestFrameworkService {
 		}
 
 	}
-	
 }
