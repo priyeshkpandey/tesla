eec20fa8ae0da58be8f7cf2fbe6ebee5a5c21b74
priyesh0001 <priyesh.pandey@akosha.in>
2016-03-30 17:41:31 +0530

    Developed callToCheckoutCallbackApi

--- src/main/java/com/hc/test/framework/keywords/AndroidKeywords.java
+++ src/main/java/com/hc/test/framework/keywords/AndroidKeywords.java
@@ -1,7 +1,78 @@
 package com.hc.test.framework.keywords;
 
-import com.hc.test.framework.core.CustomFunctions;
+import java.io.IOException;
+import java.text.MessageFormat;
 
+import org.apache.commons.configuration2.Configuration;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.hc.test.framework.core.CustomFunctions;
+import com.hc.test.framework.utils.PropertiesUtil;
+
+@Component
 public class AndroidKeywords extends CustomFunctions {
+
+	@Autowired
+	private PropertiesUtil propUtil;
+
+	static {
+		System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "DEBUG");
+	}
+
+	public static Logger LOGGER = LoggerFactory
+			.getLogger(AndroidKeywords.class);
+
+	public boolean callToCallbackApi(Configuration objRepo, WebDriver driver,
+			String objKey, String data) {
+		boolean result = true;
+
+		try {
+			String platform = data.split(",")[0];
+			String amount = data.split(",")[1];
+			String orderId = data.split(",")[2];
+			String paymentMode = data.split(",")[3];
+			Object[] checkoutCallbackParams = { platform };
+			Object[] checkoutCallbackAPIParams = { amount, orderId, paymentMode };
+			String checkoutBaseUrl = propUtil.getProperty("api.checkout.base");
+			MessageFormat checkoutCallbackFormat = new MessageFormat(
+					propUtil.getProperty("checkout.callback.success"));
+			MessageFormat paramsFormat = new MessageFormat(
+					propUtil.getProperty("checkout.callback.params"));
+			CloseableHttpClient httpClient = HttpClients.createDefault();
+			String callbackUrl = checkoutBaseUrl
+					+ checkoutCallbackFormat.format(checkoutCallbackParams)
+					+ paramsFormat.format(checkoutCallbackAPIParams);
+			HttpPost checkoutCallbackPost = new HttpPost(callbackUrl);
+			CloseableHttpResponse callbackResponse = httpClient
+					.execute(checkoutCallbackPost);
+			Integer respCode = callbackResponse.getStatusLine().getStatusCode();
+			
+			if(!respCode.toString().startsWith("20"))
+			{
+				result = false;
+				LOGGER.error("Response code for checkout callback is "+respCode);
+			}
+			
+		} catch (ClientProtocolException e) {
+			result = false;
+			LOGGER.error(e.getMessage());
+		} catch (IOException e) {
+			result = false;
+			LOGGER.error(e.getMessage());
+		} finally {
+
+		}
+
+		return result;
+	}
 
 }
