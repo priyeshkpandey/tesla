5721b84260f524df53f768608deb99217f8f8884
LP_Testing <LP_Testing@LP_Testing-PC>
2016-03-31 15:30:13 +0530

    andriod keywords

--- src/main/java/com/hc/test/framework/keywords/AndroidKeywords.java
+++ src/main/java/com/hc/test/framework/keywords/AndroidKeywords.java
@@ -1,7 +1,26 @@
 package com.hc.test.framework.keywords;
 
+import org.apache.commons.configuration2.Configuration;
+import org.openqa.selenium.WebDriver;
+
 import com.hc.test.framework.core.CustomFunctions;
+import com.hc.test.framework.utils.DriverUtils;
 
 public class AndroidKeywords extends CustomFunctions {
+	
+    DriverUtils driverUtils;
 
+    public boolean tapOnTile(Configuration objRepo, WebDriver driver, String objKey, String data) {
+        boolean isClicked=false;
+
+        try {
+
+            driverUtils = new DriverUtils(driver,objRepo,objKey);
+            driverUtils.getWebElement().click();
+            isClicked=true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return isClicked;
+    }
 }
