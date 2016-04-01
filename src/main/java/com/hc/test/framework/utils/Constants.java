d617e124a40fc6c52d1f0bd36954972a7c8be1b1
sabyasachi <sabyasachi.mishra@helpchat.in>
2016-04-01 23:11:57 +0530

    added constant file

---
+++ src/main/java/com/hc/test/framework/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.hc.test.framework.utils;
+
+/**
+ * Created by sabyasachi on 31/03/16.
+ */
+public class Constants {
+
+    public static final int DEFAULT_WAIT_TIMEOUT=20;
+    public static final long THREAD_SLEEP=5;
+    public static final int DEFAULT_POLLING_TIMEOUT=500;
+
+}
