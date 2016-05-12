47149a8813f3e37a3195f8ecbe23dd333f36a9ac
Kunwar Harish <kunwarharish.singh@onedirect.in>
2016-05-12 12:18:20 +0530

    test

--- src/main/java/com/hc/test/framework/keywords/WebKeywords.java
+++ src/main/java/com/hc/test/framework/keywords/WebKeywords.java
@@ -12,13 +12,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import java.util.regex.Pattern;
 
 import com.hc.test.framework.core.CustomFunctions;
 import com.hc.test.framework.core.KeywordInvoker;
