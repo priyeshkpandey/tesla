0cbdef5be2302a8d438e8e41ac81125f4f838d88
Kunwar Harish <kunwarharish.singh@onedirect.in>
2016-05-12 11:56:44 +0530

    commit before pull

--- src/main/java/com/hc/test/framework/keywords/WebKeywords.java
+++ src/main/java/com/hc/test/framework/keywords/WebKeywords.java
@@ -5,12 +5,21 @@ import org.apache.commons.configuration2.Configuration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.regex.Pattern;
 
 import com.hc.test.framework.core.CustomFunctions;
+import com.hc.test.framework.core.KeywordInvoker;
 
 public class WebKeywords extends CustomFunctions {
 
-	
+	static {
+		System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "DEBUG");
+	}
+
+	public static Logger LOGGER = LoggerFactory.getLogger(WebKeywords.class);
+
 	public boolean input(Configuration objRepo, WebDriver driver, String objKey, String data) {
 		boolean isSuccess = true;
 		try {
@@ -53,21 +62,7 @@ public class WebKeywords extends CustomFunctions {
 		return checkstatus;
 	}
 
-	public boolean chkbox_ischecked(Configuration objRepo, WebDriver driver, String objKey, String data) {
-		boolean checkedstatus = true;
-		try {
-			DriverUtils driverUtils = new DriverUtils(driver, objRepo, objKey);
-			checkedstatus = driverUtils.getWebElement().isSelected();
-			// Thread.sleep(10000);
-		} catch (Exception e) {
-
-			checkedstatus = false;
-			e.printStackTrace();
-		}
-
-		return checkedstatus;
-
-	}
+	
 	   	
 	public boolean closebrowser(Configuration objRepo, WebDriver driver, String objKey, String data) {
 		boolean isClosed = true;
@@ -83,6 +78,76 @@ public class WebKeywords extends CustomFunctions {
 		return isClosed;
 	}
 
-	        
+	/**
+	 * This keyword will be used to verify the text of an object 
+	 * @param objRepo
+	 * @param driver
+	 * @param objKey
+	 * @param data - complete text which need to be verified
+	 * @return
+	 */
+	public boolean verifyText(Configuration objRepo, WebDriver driver, String objKey, String data) {
+		boolean checkstatus = true;
+		try {
+			DriverUtils driverUtils = new DriverUtils(driver, objRepo, objKey);
+			WebElement ele = driverUtils.getWebElement();
+			if(ele.getText().equals(data)){
+				LOGGER.debug("Pass","Actual Value = "+ele.getText()
+						+"; Expected Value ="+data);
+			}
+			else{
+				LOGGER.debug("Fail","Actual Value = "+ele.getText()
+				+"; Expected Value ="+data);
+				
+				checkstatus = false;
+			}
+			
+		} catch (Exception e) {
+
+			checkstatus = false;
+			e.printStackTrace();
+		}
+
+		return checkstatus;
+	}
+	
+	
+	/**
+	 * This keyword will be used to verify the regular exp with an object's text 
+	 * @param objRepo
+	 * @param driver
+	 * @param objKey
+	 * @param data - complete text which need to be verified
+	 * @return
+	 */
+	public boolean verifyRegEx(Configuration objRepo, WebDriver driver, String objKey, String data) {
+		boolean checkstatus = true;
+		try {
+			DriverUtils driverUtils = new DriverUtils(driver, objRepo, objKey);
+			WebElement ele = driverUtils.getWebElement();
+			String objText = ele.getText();
+			
+			
+			
+			if(Pattern.matches(data, objText)){
+				LOGGER.debug("Pass","Actual Value = "+ele.getText()
+						+"; Expected Value ="+data);
+			}
+			else{
+				LOGGER.debug("Fail","Actual Value = "+ele.getText()
+				+"; Expected Value ="+data);
+				
+				checkstatus = false;
+			}
+			
+		} catch (Exception e) {
+
+			checkstatus = false;
+			e.printStackTrace();
+		}
+
+		return checkstatus;
+	}
+	
 
 }
